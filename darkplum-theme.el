1299582bfdf2691018c1f88986cda968d16c2ba8
rebecca skinner <rebecca@rebeccaskinner.net>
2021-05-16 00:38:25 -0500

    add autoload

--- darkplum-theme.el
+++ darkplum-theme.el
@@ -138,6 +138,10 @@
       'darkplum
       `(ansi-color-names-vector []))))
 
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'darkplum)
 
 ;; Local Variables:
